f80c5ddc1f7f2445d3b82a63bda54c879126911e
77web <info@77-web.com>
2011-10-08 15:25:11 +0900

    fixed hello-twig due to change of Extension names

--- hello-twig/index.php
+++ hello-twig/index.php
@@ -7,8 +7,8 @@ require_once dirname(__DIR__).'/silex.phar';
 $app = new Silex\Application();
 
 //twig
-use Silex\Extension\TwigExtension;
-$app->register(new TwigExtension(), array('twig.path'=>__DIR__.'/views', 'twig.class_path'=>dirname(__DIR__).'/vendor/twig/lib'));
+use Silex\Provider\TwigServiceProvider;
+$app->register(new TwigServiceProvider(), array('twig.path'=>__DIR__.'/views', 'twig.class_path'=>dirname(__DIR__).'/vendor/twig/lib'));
 
 //routings
 $app->get('/hello-twig/{name}', function($name) use ($app){ 
